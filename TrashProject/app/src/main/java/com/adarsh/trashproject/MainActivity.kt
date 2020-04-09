5cf0ba249066c024d1f61733a0f05d5af14df054
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-10 00:07:43 +0530

    Trash Completed

--- TrashProject/app/src/main/java/com/adarsh/trashproject/MainActivity.kt
+++ TrashProject/app/src/main/java/com/adarsh/trashproject/MainActivity.kt
@@ -5,18 +5,40 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var addBut:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var addBut = findViewById<Button>(R.id.button)
 
-        addBut.setOnClickListener(View.OnClickListener {
+        /* Method 1
+         addBut = findViewById<Button>(R.id.button)
+
+        //Elvis operator
+        addBut?.setOnClickListener(View.OnClickListener {
             Toast.makeText(this@MainActivity, "hello", Toast.LENGTH_SHORT).show()
         })
+
+         */
+
+
+    //import kotlinx.android.synthetic.main.activity_main.*  which automatically calls button values from xml file
+        button.setOnClickListener(View.OnClickListener {
+            val result = editText.text.toString().toInt() + editText2.text.toString().toInt()
+            textView.text = result.toString()
+        })
+
+        /* Since SetOnclickListener has only 1 function inside it.
+        //Method 3
+        button.setOnClickListener({
+            val result = editText.text.toString().toInt() + editText2.text.toString().toInt()
+            textView.text = result.toString()
+        })
+         */
 
     }
 }

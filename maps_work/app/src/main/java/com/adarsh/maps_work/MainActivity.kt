bb83eae980953866f449d241b1ad0cf2c06cb23c
Adarsh Singh <2001adarshsingh@gmail.com>
2020-05-21 08:22:41 +0530

    Maps Part 2 added

--- maps_work/app/src/main/java/com/adarsh/maps_work/MainActivity.kt
+++ maps_work/app/src/main/java/com/adarsh/maps_work/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.adarsh.maps_work
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val ERROR_DIALOG_REQUEST = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(isServicesAvailable()){
+            btn.setOnClickListener {
+                startActivity(Intent(this, Maps2Activiy::class.java))
+                finish()
+            }
+        }
+    }
+
+
+    //Checking if the user has correct version, and google play store services installed in his phone
+    private fun isServicesAvailable(): Boolean{
+        Log.d("IsServicesAvailable", "Checking google services version")
+        val available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        when {
+            available == ConnectionResult.SUCCESS -> {
+                //everything is fine, we can make request
+                Log.d("IsServicesAvailable: ", "everything is fine, we can make request")
+                return true
+            }
+            GoogleApiAvailability.getInstance().isUserResolvableError(available) -> {
+                //an error occured but we can resolve it.
+                Log.d("IsServicesAvailable", "version error occurred -> Resolvable")
+                val dialog = GoogleApiAvailability.getInstance().getErrorDialog(this@MainActivity , available, ERROR_DIALOG_REQUEST)
+                dialog.show()
+            }
+            else -> {
+                Toast.makeText(this, "You can't make map request", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return false
     }
 }
---
+++ maps_work/app/src/main/java/com/adarsh/maps_work/Maps2Activiy.kt
@@ -0,0 +1,158 @@
+package com.adarsh.maps_work
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+
+class Maps2Activiy : AppCompatActivity() , OnMapReadyCallback {
+
+    private lateinit var mMap:GoogleMap
+    private val locationManager by lazy {
+        getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps2_activiy)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map2) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onStart() {
+        super.onStart()
+        requestPermissionForLocation()
+        when{
+            checkPermission() -> {
+                when{
+                    isLocationEnabled() -> setUpLocation()  //setUpLocationListener()
+                    else -> showDialogBox()
+                }
+            }
+            else -> requestPermissionForLocation()
+        }
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+    }
+
+    private fun moveCamera(ltnlog:LatLng, zoom:Float){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ltnlog , zoom))
+    }
+
+    //Setting up different Listener
+    private fun setUpLocation(){
+        val mFusedLocationProviderClient = FusedLocationProviderClient(this)
+        val location = mFusedLocationProviderClient.lastLocation
+        location.addOnCompleteListener {
+            if(it.isSuccessful){
+                val currentLocation = it.result
+                moveCamera(LatLng(currentLocation!!.latitude, currentLocation.longitude), 15f)
+                mMap.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(this, "UnAble to get current Location", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    // Fused Location Listner
+    private fun setUpLocationListener(){
+        val fusedLocationProviderClient = FusedLocationProviderClient(this)
+        val locationRequest =  LocationRequest()
+            .setInterval(2000) //milli sec
+            .setFastestInterval(2000)
+            .setSmallestDisplacement(1f)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                for (location in locationResult.locations){
+                    val currentLocation = LatLng(location.latitude, location.longitude)
+                    if(::mMap.isInitialized){
+                        mMap.addMarker(MarkerOptions().position(currentLocation).title("Selected Marker"))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+                    }
+                }
+            }
+        }
+            , Looper.myLooper()
+        )
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkPermission(): Boolean {
+        return checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestPermissionForLocation() {
+        this.requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 123)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            123 -> if(checkPermission()) {
+                when{
+                    isLocationEnabled() -> setUpLocation() //setUpLocationListener()
+                    else -> showDialogBox()
+                }
+            } else {
+                Toast.makeText(this, "Permission of Location not given", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    //Fused Location Manager
+    private fun isLocationEnabled():Boolean{
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
+    fun showDialogBox(){
+        AlertDialog.Builder(this)
+            .setTitle("Enable GPS")
+            .setCancelable(false)
+            .setMessage("GPS is required for Google Map")
+            .setPositiveButton( "Enable Now") { dialogInterface: DialogInterface, _: Int ->
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                dialogInterface.dismiss()
+            }.show()
+    }
+
+}

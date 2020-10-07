5b3da2478d262111f8d92c95547a4762ebae9b3d
Adarsh Singh <2001adarshsingh@gmail.com>
2020-10-07 23:43:09 +0530

    WorkerManager added

---
+++ WorkManagerJetpack/app/src/main/java/com/adarsh/workmanager_jetpack/ApiManager.kt
@@ -0,0 +1,15 @@
+package com.adarsh.workmanager_jetpack
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiManager {
+    @GET("/users")
+    suspend fun getUser(): Response<List<User>>
+
+    @GET("/users/{name}")
+    suspend fun getUserByName(@Path("name") name:String): Response<User>
+
+}
---
+++ WorkManagerJetpack/app/src/main/java/com/adarsh/workmanager_jetpack/GithubWorker.kt
@@ -0,0 +1,23 @@
+package com.adarsh.workmanager_jetpack
+
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class GithubWorker(val context: Context, val param: WorkerParameters) : CoroutineWorker(context, param ){
+
+    override suspend fun doWork(): Result {
+        val response = withContext(Dispatchers.IO){ RetroFitClient.githubApi.getUser() }
+        return if(response.isSuccessful){
+            Log.e("TAG", "doWork: isSuccessful", )
+            Result.success()
+        }else{
+            Log.e("TAG", "doWork: Restarted", )
+            Result.retry()
+        }
+    }
+
+}
